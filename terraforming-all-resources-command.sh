b12259582b4d115fa6f3e2ef62cf126a7fe6bb72
Fernando Arias SAntos <fernan.fpinformatica@gmail.com>
2018-12-02 22:32:20 +0100

    Added arguments parsing support and implement terraforming command

--- terraforming-all-resources-command.sh
+++ terraforming-all-resources-command.sh
@@ -6,59 +6,73 @@ die() {
 }
 
 show_help() {
-    echo -e "Usage: ${0##*/} [--region AWS_REGION] [--profile AWS_PROFILE] [--dir DIR]...\n"
+    echo -e "Usage: ${0##*/} [--region AWS_REGION] [--profile AWS_PROFILE]\n"
     echo -e "Export all AWS existing resources to Terraform files using Terraforming tool."
     echo -e "\t--help\t\t\tdisplay this help and exit"
     echo -e "\t--region AWS_REGION\tthe AWS region you want to export from. Default: eu-west-1"
     echo -e "\t--profile AWS_PROFILE\tthe AWS profile of your ~/.aws/credentials file"
-    echo -e "\t--dir DIR\t\tdirectory where terraform resources will be saved. Default: terraform"
 }
 
-# Initialize all the option variables.
-# This ensures we are not contaminated by variables from the environment.
-file=
-verbose=0
+profile=
+region=
 
 while :; do
     case $1 in
         -h|-\?|--help)
-            show_help    # Display a usage synopsis.
+            show_help
             exit
             ;;
-        -f|--file)       # Takes an option argument; ensure it has been specified.
+        -p|--profile)
             if [ "$2" ]; then
-                file=$2
+                profile=$2
                 shift
             else
-                die 'ERROR: "--file" requires a non-empty option argument.'
+                die 'ERROR: "--profile" requires a non-empty option argument.'
             fi
             ;;
-        --file=?*)
-            file=${1#*=} # Delete everything up to "=" and assign the remainder.
+        --profile=?*)
+            profile=${1#*=}
             ;;
-        --file=)         # Handle the case of an empty --file=
-            die 'ERROR: "--file" requires a non-empty option argument.'
+        --profile=)
+            die 'ERROR: "--profile" requires a non-empty option argument.'
             ;;
-        -v|--verbose)
-            verbose=$((verbose + 1))  # Each -v adds 1 to verbosity.
+        -r|--region)
+            if [ "$2" ]; then
+                region=$2
+                shift
+            else
+                die 'ERROR: "--region" requires a non-empty option argument.'
+            fi
             ;;
-        --)              # End of all options.
-            shift
-            break
+        --region=?*)
+            region=${1#*=}
+            ;;
+        --region=)
+            die 'ERROR: "--region" requires a non-empty option argument.'
             ;;
         -?*)
             printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
             ;;
-        *)               # Default case: No more options, so break out of the loop.
+        *)
             break
     esac
 
     shift
 done
 
-# if --file was provided, open it for writing, else duplicate stdout
-if [ "$file" ]; then
-    exec 3> "$file"
+export AWS_REGION=$region
+dir=terraform_$profile_$region
+
+if [ ! -z "$profile" ] && [ ! -z "$region" ]; then
+    mkdir -p $dir
+    cd $dir
+
+    echo -e "\e[32mExporting AWS resources into $dir...\e[0m"
+    terraforming help | grep terraforming | grep -v help | awk -v aws_profile="$profile" '{print "terraforming", $2, "--profile", aws_profile, ">", $2".tf";}' | bash
+
+    echo -e "\e[32mDeleting empty terraform files...\e[0m"
+	find . -type f -name '*.tf' | xargs -i bash -c 'if [ $(wc -l {}|cut -d" " -f1) -eq 1 ]; then rm -f {}; fi'
 else
     exec 3>&1
+    exit 1
 fi
