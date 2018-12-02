71b955e47b26c5dc58e44868585aa3b66fbb996a
Fernando Arias SAntos <fernan.fpinformatica@gmail.com>
2018-12-02 22:46:47 +0100

    Extract functions to print error messages

--- terraforming-all-resources-command.sh
+++ terraforming-all-resources-command.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-die() {
-    printf '%s\n' "$1" >&2
+empty_parameter_error() {
+    printf 'ERROR: "--%s" requires a non-empty option argument.\n' "$1"
     exit 1
+}
+
+missing_parameter_error() {
+    printf 'ERROR: Missing option argument "--%s".\n' "$1"
 }
 
 show_help() {
@@ -27,28 +31,28 @@ while :; do
                 profile=$2
                 shift
             else
-                die 'ERROR: "--profile" requires a non-empty option argument.'
+                empty_parameter_error "profile"
             fi
             ;;
         --profile=?*)
             profile=${1#*=}
             ;;
         --profile=)
-            die 'ERROR: "--profile" requires a non-empty option argument.'
+            empty_parameter_error "profile"
             ;;
         -r|--region)
             if [ "$2" ]; then
                 region=$2
                 shift
             else
-                die 'ERROR: "--region" requires a non-empty option argument.'
+                empty_parameter_error "region"
             fi
             ;;
         --region=?*)
             region=${1#*=}
             ;;
         --region=)
-            die 'ERROR: "--region" requires a non-empty option argument.'
+            empty_parameter_error "region"
             ;;
         -?*)
             printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
@@ -73,7 +77,7 @@ if [ ! -z "$profile" ] && [ ! -z "$region" ]; then
     echo -e "\e[32mDeleting empty terraform files...\e[0m"
 	find . -type f -name '*.tf' | xargs -i bash -c 'if [ $(wc -l {}|cut -d" " -f1) -eq 1 ]; then rm -f {}; fi'
 else
-    if [ -z "$region" ]; then echo ERROR: Missing option argument "--region". ; fi
-    if [ -z "$profile" ]; then echo ERROR: Missing option argument "--profile". ; fi
+    if [ -z "$region" ]; then missing_parameter_error "region" ; fi
+    if [ -z "$profile" ]; then missing_parameter_error "profile" ; fi
     exit 1
 fi
